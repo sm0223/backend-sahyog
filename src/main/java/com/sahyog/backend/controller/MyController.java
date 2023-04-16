40fc86c143cd95908fc67304e377fcdad038d375
sm0223 <way2smondal@gmail.com>
2023-04-17 03:55:13 +0530

    SSE emitter implemented properly

--- src/main/java/com/sahyog/backend/controller/MyController.java
+++ src/main/java/com/sahyog/backend/controller/MyController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 
@@ -19,96 +21,100 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class MyController {
     String accessToken;
     private static CustomResponse asyncCustomResponse = new CustomResponse();
-    public List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
-    public List<SseEmitter> emitters2 = new CopyOnWriteArrayList<>();
+    private HashMap<String, SseEmitter> emitters = new HashMap<>();
+    private Util util = new Util();
     @GetMapping("/home")
     public String home()  {
         System.out.println("home");
         return "";
     }
-    //Receiving Callback APIs from ABDM
+    //Receiving Callback APIs from ABDM and dispatching SSEs
     @PostMapping("/v0.5/users/auth/on-init")
     public void onInit(@RequestBody String response) throws Exception {
-        System.out.println("Response on init " + response);
+        System.out.println("ABDM RESPONSE: ON-INIT " + response);
         String keyPath = "auth\":{\"transactionId\"";
-        String transactionId = new Util().getValueFromString(keyPath, response);
+        String transactionId = util.getValueFromString(keyPath, response);
         asyncCustomResponse.setTransactionId(transactionId);
-        for(SseEmitter emitter :emitters){
-            emitter.send((asyncCustomResponse));
-        }
+        String requestId = util.getRequestId(response);
+
+        SseEmitter sseEmitter = emitters.get(requestId);
+        sseEmitter.send(SseEmitter.event().name("ABDM-EVENT").data(asyncCustomResponse));
     }
     @PostMapping("/v0.5/users/auth/on-confirm")
     public void onConfirm(@RequestBody String response) throws Exception {
+        System.out.println("ABDM RESPONSE: ON-CONFIRM " + response);
+        accessToken = util.getAccessToken(response);
+        String requestId = util.getRequestId(response);
 
-        accessToken = Util.getAccessToken(response);
-//        post
-        System.out.println("AccessToken arrived  " + accessToken);
-
-        System.out.println("Response on confirm " + response);
         String keyPath1 = "patient\":";
         String keyPath2 = "\"error\"";
-        String patient = new Util().getValueFromString2(keyPath1,keyPath2, response);
-
+        String patient = util.getValueFromString2(keyPath1,keyPath2, response);
         asyncCustomResponse.setPatient(patient);
-        for(SseEmitter emitter :emitters2){
-            emitter.send((asyncCustomResponse));
-        }
+
+        SseEmitter sseEmitter = emitters.get(requestId);
+        sseEmitter.send(SseEmitter.event().name("ABDM-EVENT").data(asyncCustomResponse));
     }
 
-    //Custom APIS for FRONTEND
-    @PostMapping(value = "/api/register/health-id")
+    //Custom APIS for FRONTEND and Initiating SSEs
+    @PostMapping(value = "/api/register/health-id") // auth/init
     public SseEmitter registerPatientUsinghealthId(@RequestBody CustomRequest customRequest) throws Exception, IOException {
-        System.out.println("Patient Abha Address : " + customRequest.getHealthId());
+        System.out.println("REQUEST: REGISTER-PATIENT-USING-HEALTH-ID");
         ABDMSession session = new ABDMSession();
         session.setToken();
-        System.out.println("retreived token : " + session.getToken());
-        int statusCode = session.patientInitUsingMobile(customRequest.getHealthId()); //Calling abha to init patient using mobile otp
-        SseEmitter sseEmitter = new SseEmitter((long)1000);
-        if (statusCode == 202) {
-//            sseEmitter.send(SseEmitter.event().name("INIT"));
-            System.out.println("INIT Patient using mobile (sent to abha): " + statusCode);
-        } else {
-            System.out.println("Authentication Error : " + statusCode);
-        }
+        String UUIDCode = UUID.randomUUID().toString();
+        int statusCode = session.patientInitUsingMobile(UUIDCode, customRequest.getHealthId()); //Calling abha to init patient using mobile otp
+
+        SseEmitter sseEmitter = new SseEmitter((long)5000);
+        emitters.put(UUIDCode, sseEmitter);
         sseEmitter.onCompletion(()->emitters.remove(sseEmitter));
-        emitters.add(sseEmitter);
+        sseEmitter.onTimeout(()->emitters.remove(sseEmitter));
+        sseEmitter.onError((e)->emitters.remove(sseEmitter));
+
+        System.out.println("STATUS: REGISTER-PATIENT-USING-HEALTH-ID: " + statusCode);
+
         return sseEmitter;
     }
 
-
-
     @PostMapping(value = "/api/register/confirmMobileOTP")
     public SseEmitter confirmMobileOTP(@RequestBody CustomRequest customRequest) throws Exception {
-        System.out.println(customRequest.toString());
-        System.out.println("Patient Abha Address : " + customRequest.getHealthId());
+        System.out.println();
+
         ABDMSession session = new ABDMSession();
         session.setToken();
-        System.out.println("retreived token : " + session.getToken());
-        int statusCode = session.confirmMobileOTP(customRequest.getTransactionId(), customRequest.getMobileOTP());
-        System.out.println("COmfirm Mobbile OTP Status Code: " + statusCode);
+        String UUIDCode = UUID.randomUUID().toString();
 
-        SseEmitter sseEmitter = new SseEmitter((long)3000);
-        sseEmitter.onCompletion(()->emitters2.remove(sseEmitter));
-        emitters2.add(sseEmitter);
+        int statusCode = session.confirmMobileOTP(UUIDCode, customRequest.getTransactionId(), customRequest.getMobileOTP());
+
+        SseEmitter sseEmitter = new SseEmitter((long)5000);
+        emitters.put(UUIDCode, sseEmitter);
+        sseEmitter.onCompletion(()->emitters.remove(sseEmitter));
+        sseEmitter.onTimeout(()->emitters.remove(sseEmitter));
+        sseEmitter.onError((e)->emitters.remove(sseEmitter));
+
+        System.out.println("STATUS: REGISTER-PATIENT-USING-HEALTH-ID: " + statusCode);
         return sseEmitter;
 
 
     }
 
     @GetMapping(value = "/api/link/care-context")
-    public SseEmitter linkingCareContext() throws Exception, IOException {
+    public SseEmitter linkingCareContext() throws Exception {
+        System.out.println("REQUEST: LINKING_CARE_CONTEXT");
         ABDMSession session = new ABDMSession();
         session.setToken();
-        System.out.println("retreived token : " + session.getToken());
+
         int statusCode = session.careContextLinking(accessToken);
+        String UUIDCode = UUID.randomUUID().toString();
         SseEmitter sseEmitter = new SseEmitter((long)1000);
-        if (statusCode == 202) {
-            System.out.println("INIT Patient using mobile (sent to abha): " + statusCode);
-        } else {
-            System.out.println("Authentication Error : " + statusCode);
-        }
+
         sseEmitter.onCompletion(()->emitters.remove(sseEmitter));
-        emitters.add(sseEmitter);
+        emitters.put(UUIDCode, sseEmitter);
+        sseEmitter.onCompletion(()->emitters.remove(sseEmitter));
+        sseEmitter.onTimeout(()->emitters.remove(sseEmitter));
+        sseEmitter.onError((e)->emitters.remove(sseEmitter));
+
+        System.out.println("STATUS: LINKING_CARE_CONTEXT: " + statusCode);
+
         return sseEmitter;
     }
 
@@ -133,7 +139,7 @@ public class MyController {
     @PostMapping("/api/patient/{healthId}")
     public Patient getPatient(@PathVariable String healthId)
     {
-        System.out.println("asdf "+ healthId);
+        System.out.println("Getting Patient from HealthId "+ healthId);
         return patientService.findPatientByHealthId(healthId);
     }
 
@@ -218,8 +224,6 @@ public class MyController {
     {
         return adminStaffService.updateStaff(staff);
     }
-
-
 
 }
 
